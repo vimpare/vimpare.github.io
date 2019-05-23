6aad96a986ecdec97f849d1d9efdb243e14beda6
Yuer <1689416292@qq.com>
2019-05-23 20:20:53 +0800

    Create menuList.js

---
+++ js/menuList.js
@@ -0,0 +1,41 @@
+let menuList=[{
+    title: 'vue学习',
+    href: 'https://github.com/vimpare/vue-test',
+    childrens: [{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    },{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    },{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    }]
+}, {
+    title: 'vue学习',
+    href: 'https://github.com/vimpare/vue-test',
+    childrens: [{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    }]
+},{
+    title: 'vue学习',
+    href: 'https://github.com/vimpare/vue-test',
+    childrens: [{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    }]
+},{
+    title: 'vue学习',
+    href: 'https://github.com/vimpare/vue-test',
+    childrens: [{
+        title: '安装，插件引入及axios',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        childrens: []
+    }]
+}]
