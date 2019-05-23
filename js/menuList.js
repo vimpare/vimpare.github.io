d3f812794637021dfe54a8851daf085a8ca504f8
Yuer <1689416292@qq.com>
2019-05-23 20:27:28 +0800

    Update menuList.js

--- js/menuList.js
+++ js/menuList.js
@@ -15,11 +15,11 @@ let menuList=[{
         childrens: []
     }]
 }, {
-    title: 'vue学习',
+    title: 'JavaScript设计模式与开发实践/',
     href: 'https://github.com/vimpare/vue-test',
     childrens: [{
-        title: '安装，插件引入及axios',
-        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E5%AE%89%E8%A3%85%EF%BC%8C%E6%8F%92%E4%BB%B6%E5%BC%95%E5%85%A5%20axios.md',
+        title: '职责链模式',
+        href: 'https://github.com/vimpare/note/blob/master/JavaScript%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F%E4%B8%8E%E5%BC%80%E5%8F%91%E5%AE%9E%E8%B7%B5/%E8%81%8C%E8%B4%A3%E9%93%BE%E6%A8%A1%E5%BC%8F.md',
         childrens: []
     }]
 },{
