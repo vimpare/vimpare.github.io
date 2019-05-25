dd5fca0c5b6d6912d10182b9efc0250310cc8e23
Yuer <1689416292@qq.com>
2019-05-25 09:43:12 +0800

    Update menuList.js

--- js/menuList.js
+++ js/menuList.js
@@ -21,6 +21,14 @@ let menuList=[{
         title: '职责链模式',
         href: 'https://github.com/vimpare/note/blob/master/JavaScript%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F%E4%B8%8E%E5%BC%80%E5%8F%91%E5%AE%9E%E8%B7%B5/%E8%81%8C%E8%B4%A3%E9%93%BE%E6%A8%A1%E5%BC%8F.md',
         childrens: []
+    },{
+        title: 'vue生命周期',
+        href: 'https://github.com/vimpare/vue-test/blob/master/vue%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F.md',
+        childrens: []
+    },{
+        title: '内置的组件 特殊属性',
+        href: 'https://github.com/vimpare/vue-test/blob/master/%E5%86%85%E7%BD%AE%E7%9A%84%E7%BB%84%E4%BB%B6%20%E7%89%B9%E6%AE%8A%E5%B1%9E%E6%80%A7.md',
+        childrens: []
     }]
 },{
     title: 'vue学习',
